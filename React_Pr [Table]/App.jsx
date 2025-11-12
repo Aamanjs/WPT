55f2f25a965806d53598eb484cd6e779b3a195d9
Aaman Sayyad <86286688+Aamanjs@users.noreply.github.com>
2025-11-13 00:23:50 +0530

    Create App component with Product import

---
+++ React_Pr [Table]/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Product from './Components/Product'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Product/>
+    </>
+  )
+}
+
+export default App
