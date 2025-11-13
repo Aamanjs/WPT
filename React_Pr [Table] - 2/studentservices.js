1a982efd8997a915dc4c95f10dd3e9794e9ec8ad
Aaman Sayyad <86286688+Aamanjs@users.noreply.github.com>
2025-11-13 14:57:00 +0530

    Implement StudentService for student CRUD operations

---
+++ React_Pr [Table] - 2/studentservices.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const base_url = "http://localhost:1234/students"
+
+export const StudentService = {
+
+    getAll: async() => (await axios.get(base_url)).data,
+
+    getById: async(id) => (await axios.get(`${base_url}/${id}`)).data,
+
+    add: async(rec) => (await axios.post(base_url, rec)).data,
+
+    update: async(rec) => (await axios.put(`${base_url}/${rec.id}`, rec)).data,
+
+    delete: async(id) => (await axios.delete(`${base_url}/${id}`)).data
+
+}
