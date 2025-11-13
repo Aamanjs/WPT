fcfa4985de5ee10437d0e69f6db915652462ad4f
Aaman Sayyad <86286688+Aamanjs@users.noreply.github.com>
2025-11-13 14:59:33 +0530

    Create App component with Student import

---
+++ React_Pr [Table] - 2/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Student from './Components/Students'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Student/>
+    </>
+  )
+}
+
+export default App
